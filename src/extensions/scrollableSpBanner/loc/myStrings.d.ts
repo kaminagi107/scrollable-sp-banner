01ae4bcbe3d75f386af43f11d302cc8b89b3422c
kaminagi107 <xaml.kaminagi107-amg@live.jp>
2018-07-01 22:43:52 +0900

    feat: Initialize project.

---
+++ src/extensions/scrollableSpBanner/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IScrollableSpBannerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ScrollableSpBannerApplicationCustomizerStrings' {
+  const strings: IScrollableSpBannerApplicationCustomizerStrings;
+  export = strings;
+}
